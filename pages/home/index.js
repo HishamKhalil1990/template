768de568e1d25b63f0f696716fe8a1552b360861
hishamAbuodeh <H.Ibraheem@abuodeh.com>
2022-09-17 15:59:01 +0300

    home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,52 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from "next/router"
+import AsyncLocalStorage from '@createnextapp/async-local-storage'
 export default function Home(props){
+
+    const router = useRouter()
+
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const checkLogin = async() => {
+            try {
+            let tokens = await AsyncLocalStorage.getItem('tokens')
+            if(tokens){
+                tokens = JSON.parse(tokens)
+                let day = new Date();
+                day = day.getDay();
+                if(tokens.day != day){
+                    await AsyncLocalStorage.removeItem('tokens')
+                    router.push('/account')
+                }else{
+                    setLoading(false)
+                }
+            }else{
+                router.push('/account')
+            }
+            }catch(err){
+                router.push('/account')
+            }
+        }
+        checkLogin()
+    },[])
+
+    const HomeLayout = () => {
+        return(
+            <div>
+                home page
+            </div>
+        )
+    } 
+
     return(
-        <div>
-            home page
-        </div>
+        <>
+            {
+            loading?
+                <div>loading</div>
+            :
+                <HomeLayout/>
+            }
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from "next/router"
+import AsyncLocalStorage from '@createnextapp/async-local-storage'
 import Account from './account'
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkLogin = async() => {
+      try {
+        let tokens = await AsyncLocalStorage.getItem('tokens')
+        if(tokens){
+          tokens = JSON.parse(tokens)
+          let day = new Date();
+          day = day.getDay();
+          if(tokens.day == day){
+            router.push('/home')
+          }else{
+            await AsyncLocalStorage.removeItem('tokens')
+            setLoading(false)
+          }
+        }else{
+          setLoading(false)
+        }
+      }catch(err){
+        setLoading(false)
+      }
+    }
+    checkLogin()
+  },[])
+
   return (
-    <Account/>
+    <>
+      {
+      loading?
+        <div>loading</div>
+      :
+        <Account/>
+      }
+    </>
   )
 }
